fdce6f36d31f2f028fcdce5c9a3d8ffa93a58d57
sahsisunny <sahsisunny@gmail.com>
2022-09-29 02:44:54 +0530

    all required files added for projects

--- footer.php
+++ footer.php
@@ -1,12 +1,18 @@
   <footer class="footer-2">
     <div class="container">
       <div class="row">
+
+        <!-- Support -->
         <div class="col-sm-7">
           <h6>About Us</h6>
           <div class="about-foot">
             <p>A concern of Care and Cure Group is the only multi-disciplinary super-specialty tertiary care hospital in Bangladesh, confidently providing comprehensive health care with the latest medical, surgical and diagnostic facilities. These services are provided by expert medical professionals, skilled nurses and technologists using state-of-the-art technology. </p>
           </div>
         </div>
+
+
+
+        <!-- Conatct -->
         <div class="col-sm-5">
           <div class="con-info">
             <h6>Contact Us</h6>
@@ -25,13 +31,21 @@
       </div>
     </div>
   </footer>
+
+  <!-- Rights -->
   <div class="rights style-2">
     <div class="container">
-      <p>© <?php echo date("Y"); ?> Hospital Management System Patna, Developed By- <span style="color: white">Sunny Sahsi</span></p>
+      <p>© <?php echo date("Y"); ?> Hospital Management System Patna, Developed By-  <span style="color: white">Sunny Sahsi</span></p>
     </div>
   </div>
+
+  <!-- GO TO TOP  -->
   <a href="#" class="cd-top"><i class="fa fa-angle-up"></i></a>
+  <!-- GO TO TOP  -->
   </div>
+  <!-- End Page Wrapper -->
+
+  <!-- JavaScripts -->
   <script src="js/vendors/jquery/jquery.min.js"></script>
   <script src="js/vendors/wow.min.js"></script>
   <script src="js/vendors/bootstrap.min.js"></script>
@@ -39,8 +53,11 @@
   <script src="js/vendors/jquery.sticky.js"></script>
   <script src="js/vendors/owl.carousel.min.js"></script>
   <script src="js/vendors/jquery.magnific-popup.min.js"></script>
+
+  <!-- SLIDER REVOLUTION 4.x SCRIPTS  -->
   <script type="text/javascript" src="rs-plugin/js/jquery.tp.t.min.js"></script>
   <script type="text/javascript" src="rs-plugin/js/jquery.tp.min.js"></script>
   <script src="js/main.js"></script>
   </body>
+
   </html>
